015adc9ae158f16d05486c4655bd0743f861681a
Abdullahtariq11 <abdullahtariq096@gmail.com>
2025-02-27 20:24:16 -0800

    Comit

---
+++ MomentOfUs.App/obj/Debug/net8.0-android/android/src/crc64d6ba2cdc1548f1c8/MainActivity.java
@@ -0,0 +1,48 @@
+package crc64d6ba2cdc1548f1c8;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("MomentOfUs.App.MainActivity, MomentOfUs.App", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("MomentOfUs.App.MainActivity, MomentOfUs.App", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("MomentOfUs.App.MainActivity, MomentOfUs.App", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
