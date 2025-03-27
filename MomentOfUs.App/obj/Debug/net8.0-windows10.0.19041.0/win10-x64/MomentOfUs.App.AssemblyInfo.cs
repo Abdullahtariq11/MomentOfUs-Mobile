964598dc699c89f0c1c11a301d1c91421ef7e6b3
Abdullahtariq11 <abdullahtariq096@gmail.com>
2025-03-26 23:15:03 -0700

    minor updates

--- MomentOfUs.App/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MomentOfUs.App.AssemblyInfo.cs
+++ MomentOfUs.App/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MomentOfUs.App.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MomentOfUs.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+be334273d4d975ec7b889c252cce570e4b9101e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0dc4886521e5f8e3899baaa9ec0dd0b46c7202c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MomentOfUs.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MomentOfUs.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
