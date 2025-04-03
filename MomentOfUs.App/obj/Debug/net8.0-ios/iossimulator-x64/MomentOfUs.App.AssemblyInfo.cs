6ece67975f452449a679ac9e8d93c0cfdf22d563
Abdullahtariq11 <abdullahtariq096@gmail.com>
2025-04-02 21:14:18 -0700

    static pages completed

--- MomentOfUs.App/obj/Debug/net8.0-ios/iossimulator-x64/MomentOfUs.App.AssemblyInfo.cs
+++ MomentOfUs.App/obj/Debug/net8.0-ios/iossimulator-x64/MomentOfUs.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MomentOfUs.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+964598dc699c89f0c1c11a301d1c91421ef7e6b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f8d93d83bc9a17b11a4a5bcd6f3f2c74c18ef0c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MomentOfUs.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MomentOfUs.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
